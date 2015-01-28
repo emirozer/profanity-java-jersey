737ce8a23729b48b0cf616d095bd53c5fb6059ad
Emir Ozer <emirozer@yandex.com>
2015-01-28 11:34:48 +0100

    initial commit

---
+++ src/main/java/io/ghost/emirozer/Filter.java
@@ -0,0 +1,154 @@
+package io.ghost.emirozer;
+
+import javax.ws.rs.*;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.logging.Logger;
+
+
+/**
+ * Root resource (exposed at "filter" path)
+ */
+@Path("filter")
+public class Filter {
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+
+    @GET
+    @Consumes("text/plain")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String filterIt(@QueryParam("input") String toBeFiltered,
+                           @QueryParam("lang") String lang) {
+
+        final Logger logger = Logger.getLogger(Filter.class.getName());
+
+        String [] langArray = new String[22];
+        langArray = new String[]{"cs", "da", "de", "en", "eo", "es", "fi", "fr", "hu", "it",
+                "ja", "ko", "nl", "no", "pl", "pt", "ru", "sv", "th", "tlh", "tr", "zh"};
+
+        String final_result = "";
+        String filtered = "";
+        String[] list_splitted = toBeFiltered.split(" ");
+
+        if (toBeFiltered.equals("")){
+
+            return "Empty string cannot be filtered!";
+
+        }else {
+
+            logger.warning("Received query param : input ---> " + toBeFiltered);
+
+        }
+
+        if (lang.equals("") || lang.isEmpty()){
+
+            logger.warning("Did not received query param : lang ---> defaults to \"en\" ");
+
+            for (int i=0; i < list_splitted.length; i++) {
+                filtered = filteringProcessor("", list_splitted[i]);
+
+                list_splitted[i] = filtered;
+            }
+        }else {
+            logger.warning("Received query param : lang ---> " + lang);
+
+            for(String lang_code : langArray){
+                if (lang_code.equals(lang)) {
+                for (int i=0; i < list_splitted.length; i++){
+                    // Check if the lang query param matches any lang_code in langArray
+
+                        // check for that specific file
+                        filtered = filteringProcessor(lang, list_splitted[i]);
+
+                        list_splitted[i] = filtered;
+
+                    }
+                }
+            }
+
+        }
+
+
+        for(String sp : list_splitted){
+            final_result += sp + " ";
+        }
+        final_result = final_result.substring(0, final_result.length() - 1);
+        logger.warning(final_result);
+        return final_result;
+    }
+
+    public String filteringProcessor(String langToBeFound, String inputToBeFiltered){
+
+        final Logger logger = Logger.getLogger(Filter.class.getName());
+
+        String folderPath = "src/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/";
+
+        java.nio.file.Path path = Paths.get(folderPath, langToBeFound);
+        Charset charset = Charset.forName("ISO-8859-1");
+        String line;
+        String temp = "";
+
+        if (langToBeFound.equals("")){
+
+            File dir = new File(folderPath);
+            File[] directoryListing = dir.listFiles();
+
+            if (directoryListing != null) {
+
+                for (File child : directoryListing) {
+
+                    // Do something with child
+                    String path_to_child = child.getAbsolutePath();
+                    java.nio.file.Path path_to_specific = Paths.get(path_to_child);
+
+                    try (BufferedReader reader = Files.newBufferedReader(path_to_specific, charset)) {
+                        while ((line = reader.readLine()) != null ) {
+                            if(line.equals(inputToBeFiltered)){
+                                for(int x=0; x< inputToBeFiltered.length();x++){
+                                        temp += "*";
+                                     }
+                            return temp;
+                            }
+                        }
+                    } catch (IOException e) {
+                        logger.severe(String.valueOf(e));
+                    }
+                }
+
+            } else {
+                logger.severe("Curse word dictionary files cannot be found! PLEASE TAKE ACTION!");
+            }
+
+        }else{
+            try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+
+                while ((line = reader.readLine()) != null ) {
+
+                    if(line.equals(inputToBeFiltered)){
+
+                        for(int x=0; x< inputToBeFiltered.length();x++){
+                        temp += "*";
+
+                    }
+                        return temp;
+                    }
+                }
+            } catch (IOException e) {
+                logger.severe(String.valueOf(e));
+            }
+        }
+
+
+        return inputToBeFiltered;
+    }
+
+}
---
+++ src/test/java/io/ghost/emirozer/FilterTest.java
@@ -0,0 +1,84 @@
+package io.ghost.emirozer;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FilterTest {
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        // uncomment the following line if you want to enable
+        // support for JSON in the client (you also have to uncomment
+        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+        // --
+        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+
+        target = c.target(Main.BASE_URI);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testFilterItPositive() {
+        String responseMsg = target.path("filter").queryParam("input", "This is certainly a clean input!").queryParam("lang", "en").request().get(String.class);
+        assertEquals("This is certainly a clean input!", responseMsg);
+    }
+
+    @Test
+    public void testFilterItOneWordNegative() {
+        String responseMsg = target.path("filter").queryParam("input", "shit").queryParam("lang", "en").request().get(String.class);
+        assertEquals("****", responseMsg);
+    }
+
+    @Test
+    public void testFilterItSentenceNegative() {
+        String responseMsg = target.path("filter").queryParam("input", "Emir is an asshole").queryParam("lang", "en").request().get(String.class);
+        assertEquals("Emir is an *******", responseMsg);
+    }
+
+    @Test
+    public void testFilterItSentenceNegativeMid() {
+        String responseMsg = target.path("filter").queryParam("input", "Emir is an asshole for sure!").queryParam("lang", "en").request().get(String.class);
+        assertEquals("Emir is an ******* for sure!", responseMsg);
+    }
+
+    @Test
+    public void testFilterItPositiveOneWord() {
+        String responseMsg = target.path("filter").queryParam("input", "input").queryParam("lang", "en").request().get(String.class);
+        assertEquals("input", responseMsg);
+    }
+
+    @Test
+    public void testFilterItMultipleCurses() {
+        String responseMsg = target.path("filter").queryParam("input", "shit asshole").queryParam("lang", "en").request().get(String.class);
+        assertEquals("**** *******", responseMsg);
+    }
+
+    @Test
+    public void testFilterItMultipleCursesInLongSentence() {
+        String responseMsg = target.path("filter").queryParam("input", "shit emir hehehe asshole").queryParam("lang", "en").request().get(String.class);
+        assertEquals("**** emir hehehe *******", responseMsg);
+    }
+}
